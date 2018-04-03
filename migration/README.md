26f550d2d0fce20aa9c094cd5e18919a5371fb7d
Matthew Noseworthy <mjn327@mun.ca>
2018-04-02 23:17:53 -0400

    Actual data idea that we can test with

--- migration/README.md
+++ migration/README.md
@@ -1,8 +1,6 @@
 # Migartion to firestore
 This directory will only be present on this current working branch, and will document the migration of our current data from Google Real Time Database to Google Firestore.
 
-# Value Prop
-
 # Design
 
 ## Current structure
@@ -152,10 +150,139 @@ Data is referenced by it's location in the database, just like the realtime data
 
 ## Test results
 To test my theory's of the structure from inspection, I exported the database as json and ran it through some python:
+```
+UID Key Report:
+ - Private UID's are missing from the public table
+ - Private UID's are missing from the shared table
+ - All public UID's are in the shared table
+ - Shared UID's are missing from the public table
+ - Some private member entries are missing public UID's
+ - Public UID's from private table are missing from the public table
+ - All public UID's from the private table are key's in the shared table
+ - All public UID's are present in either shared or public tables as keys
+ - All members in the priavte table have a privacy setting
+ - All members with privacy=shared have their public ID's as keys in the shared table
+ - Some members with privacy=public do not have their public ID's as keys in the public table
 
+================================================
+Data Normalization Report:
+
+Count of fields accress all entries in the Private table:
+{u'ambassador': 429,
+ u'approved': 429,
+ u'comments': 412,
+ u'current_address': 429,
+ u'date_created': 429,
+ u'email': 429,
+ u'first_name': 429,
+ u'grad_year': 64,
+ u'hometown_address': 429,
+ u'industry': 429,
+ u'interests': 429,
+ u'last_name': 429,
+ u'linkedin_profile': 429,
+ u'privacy': 429,
+ u'program': 64,
+ u'public_uid': 420,
+ u'school': 64,
+ u'status': 429}
+
+Count of fields accress all entries in the Public table:
+{u'ambassador': 248,
+ u'current_address': 248,
+ u'date_created': 248,
+ u'first_name': 248,
+ u'grad_year': 43,
+ u'hometown_address': 248,
+ u'industry': 248,
+ u'last_name': 248,
+ u'linkedin_profile': 248,
+ u'program': 43,
+ u'public_uid': 14,
+ u'school': 43,
+ u'status': 248}
+
+Count of fields accress all entries in the Shared table:
+{u'ambassador': 420,
+ u'current_address': 420,
+ u'date_created': 420,
+ u'first_name': 420,
+ u'grad_year': 61,
+ u'hometown_address': 420,
+ u'industry': 420,
+ u'last_name': 420,
+ u'linkedin_profile': 420,
+ u'program': 61,
+ u'public_uid': 124,
+ u'school': 61,
+ u'status': 420}
+```
+
+To summarize:
+ - All member's have data in the private table
+ - Members are assigned a public UID
+ - All members have entries in the shared table corresponding to their public UID
+ - If a member's private data has a privacy field, it's always set to public
+ - If privacy=public, they get an entry in the public table
+ - Fields present in public and shared fields are identical, and therefore the only purpose of the separation is for access permissions 
 
 ## Design questions for figuring out data structure
 
-Wanted Queries:
- - 
+How to determine a user's access level?
+ - The old database structure provides four access levels:
+        - Admin
+        - Private
+        - Public
+        - Shared
 
+How to restrict access to data based on user's access level?
+ - Could potentially control this client-side by reading a user's role from the database, but of course from a security perspective this is insanely weak.
+ - Could segment Public/Private data into separate documents, as firestore cannot resrict access to specific fields but only entire documents. The problem is querying through private/public data combined becomes a little more difficult and updating through those fields also becomes a little more difficult
+         - Can either use Batch updates to atomically update both documents at once 
+         - Or sequentially update all the fields
+ - Likely the best thing to do is to keep a table of administrators as is in the current structure in a separate collection, as well as the user's shared/public preference in their private data table 
+ - Restirct access to a user's private data to only their own UID or an admin ? **I forget if private data can be accessable to other private users ?**
+
+
+Wanted Queries:
+ - Get all members with field = x
+ - Get all shared//public//private members pagnated
+
+
+
+## Possible data structures
+
+### First Idea
+Major Changes:
+ - Remove public_uid 
+ - Collection of members where document name == uid of member
+ - Each document has a subdocument called private_data containing private data
+ - Instead of public_uid we add a field stating weather or not they want their profile public/priavte/shared
+ - Collection with admin UID's
+**collection**:members
+    **document**:<member uid>
+        member_type (Text String)  == 
+        ambassador      (Boolean)
+        current_address (Map | document)
+        date_created (Datetime)
+        first_name (Text String)
+        grad_year (Int)
+        hometown_address (Map | document)
+        industry (Text String)
+        last_name (Text String)
+        linkedin_profile ( Text String )
+        program ( Text String )
+        school ( Text String)
+        status ( Text String )
+        **document**:private_data
+            privacy ( Text String ) " Public | Private | Shared "
+            interests (Map | document)
+            approved ( Boolean )
+            email ( Text String )
+**collection** : administrators
+    **document**:admin_accounts
+        uids (Text String Array)
+*or*
+**collection** : administrators
+    **document**:<member uid>
+        some junk data maybe?
--- migration/db_test.py
+++ migration/db_test.py
@@ -3,6 +3,7 @@
 """
 
 import json
+import pprint
 
 # Open that dank database
 with open("/home/matthew/Downloads/globalnl-database-export.json") as f:
@@ -61,8 +62,109 @@ with open("/home/matthew/Downloads/globalnl-database-export.json") as f:
             print("{} from shared table is missing from the public table".format(shared_uid))
             shared_uid_in_public = False
     
+    ####################################################################
+    #   Public UID's stored in private table are Keys of public/shared 
+    #   table
+    ####################################################################
+    public_uid_keys_of_public = True
+    public_uid_keys_of_shared = True
+    public_uid_in_either_public_or_shared = True
+    private_table_always_has_public_uid = True
+    privacy_either_public_or_shared = True
+    for private_uid, private_data in private_members.items():
+        if private_data:
+            if u"public_uid" in private_data:
+                public_uid = private_data["public_uid"]
+            else:
+                private_table_always_has_public_uid = False
+            if not public_uid in public_members:
+                print("Public UID {} missing from public members").format(public_uid)
+                public_uid_keys_of_public = False
+            if not public_uid in shared_members:
+                print("Public UID {} missing from shared members").format(public_uid)
+                public_uid_keys_of_shared = False
+            if not public_uid in public_members and not public_uid in shared_members:
+                privacy_either_public_or_shared = False
+        else:
+            print("Public UID {} missing from both shared and public").format(public_uid)
+            private_table_always_has_public_uid = False
+    
+    ####################################################################
+    #   Members with privacy=public are in public, shared are in shared
+    #   and not in both at once
+    ####################################################################
+    privacy_shared_in_shared = True
+    privacy_public_in_public = True
+    all_members_have_privacy_setting = True
+    
+    for private_uid, private_data in private_members.items():
+        if private_data:
+            if u"privacy" in private_data:
+                if u"public_uid" in private_data:
+                    if private_data["privacy"] == u"public":
+                        if not private_data["public_uid"] in public_data:
+                            privacy_public_in_public = False 
+                    elif private_data["privacy"] == u"shared":
+                        if not private_data["public_uid"] in shared_data:
+                            privacy_shared_in_shared = False
+                    else:
+                        privacy_either_public_or_shared = False
+            else:
+                all_members_have_privacy_setting = False
+    ####################################################################
+    #   Count field occurances in each dataset
+    ####################################################################
+    private_entry_compare = {}
+    # We're just going to iterate over each member entry and increment a
+    # a counter for each field to determine what fields are missing from
+    # some entries
+    for private_uid, private_data in private_members.items():
+        if private_data:
+            for field, value in private_data.items():
+                if field in private_entry_compare:
+                    private_entry_compare[field] = private_entry_compare[field] + 1
+                else:
+                    private_entry_compare[field] = 1
+
+
+    public_entry_compare = {}
+    for public_uid, public_data in public_members.items():
+        if public_data:
+            for field, value in public_data.items():
+                if field in public_entry_compare:
+                    public_entry_compare[field] = public_entry_compare[field] + 1
+                else:
+                    public_entry_compare[field] = 1
+
+    shared_entry_compare = {}
+    for shared_uid, shared_data in shared_members.items():
+        if shared_data:
+            for field, value in shared_data.items():
+                if field in shared_entry_compare:
+                    shared_entry_compare[field] = shared_entry_compare[field] + 1
+                else:
+                    shared_entry_compare[field] = 1
+    
+
+
+    ####################################################################
+    #   Check which fields from private table are missing from shared
+    #   or public
+    ####################################################################
+    missing_field_count_private = {}
+    missing_field_count_public = {}
+    #for private_uid, private_data in private_members.items():
+        # On first iteration populate fields to check for
+    #    if missing_field_count_private = {}:
+
+    #    for public_uid
+
+
+
+    ####################################################################
     # Report UID reflection results
-    print("\n================================================\n")
+    ####################################################################
+    print("\n================================================\n\nUID Key Report:")
     if  not private_uid_in_public :
         print(" - Private UID's are missing from the public table")
     else:
@@ -79,4 +181,42 @@ with open("/home/matthew/Downloads/globalnl-database-export.json") as f:
         print(" - Shared UID's are missing from the public table")
     else:
         print(" - All shared UID's are in the public table")
-    
+    if private_table_always_has_public_uid:
+        print(" - All entries in the private table have a public UID property") 
+    else:
+        print(" - Some private member entries are missing public UID's")
+    if public_uid_keys_of_public:
+        print(" - All public UID's from the private table are key's in the public table")
+    else:
+        print( " - Public UID's from private table are missing from the public table")
+    if public_uid_keys_of_shared:
+        print(" - All public UID's from the private table are key's in the shared table")
+    else:
+        print( " - Public UID's from private table are missing from the shared table")
+    if public_uid_in_either_public_or_shared:
+        print(" - All public UID's are present in either shared or public tables as keys")
+    else:
+        print( " - Some public UID's aren't present in either shared or public tables")
+    if all_members_have_privacy_setting:
+        print(" - All members in the priavte table have a privacy setting")
+    else:
+        print(" - Some members in the private table don't have a privacy setting")
+    if privacy_shared_in_shared:
+        print(" - All members with privacy=shared have their public ID's as keys in the shared table")
+    else:
+        print(" - Some members with privacy=shared do not have their public ID's as keys in the shared table")
+    if privacy_public_in_public:
+        print(" - All members with privacy=public have their public ID's as keys in the public table")
+    else:
+        print(" - Some members with privacy=public do not have their public ID's as keys in the public table")
+
+    ####################################################################
+    # Report UID reflection results
+    ####################################################################
+    print("\n================================================\nData Normalization Report:")
+    print("\nCount of fields accress all entries in the Private table:")
+    pprint.pprint(private_entry_compare)
+    print("\nCount of fields accress all entries in the Public table:")
+    pprint.pprint(public_entry_compare)
+    print("\nCount of fields accress all entries in the Shared table:")
+    pprint.pprint(shared_entry_compare)
